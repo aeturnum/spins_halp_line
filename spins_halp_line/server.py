3b66d97ed9a709dc8c87f9493d467d433da08df8
Daniel Drexler <aeturnum@gmail.com>
2021-01-11 12:06:47 -0800

    Basic debug interface

--- spins_halp_line/server.py
+++ spins_halp_line/server.py
@@ -116,26 +116,70 @@ async def main_number():
 async def debug_interface():
     return f"""
         <head>
-            <link rel="stylesheet" type="text/css" href="/css/main.css">
             <link rel="stylesheet" type="text/css" href="/css/jsonview.bundle.css">
+            <link rel="stylesheet" type="text/css" href="/css/main.css">
             <script src="/js/jsonview.bundle.js"></script>
             <script src="/js/umbrella.min.js"></script>
         </head>
         <body>
             <main class="wrapper">
-                <div class="left" id="players"></div>
+                <div class="left">
+                    <table id="players">
+                    </table>
+                </div>
                 <div class="right" id="json"></div>
             </main>
         <script>
-            fetch('/players')
-                .then(res => res.json())
-                .then(
-                    data => {{
-                        u("#players).append(plr => `<p>${{ plr }}</p>`, Object.keys(data));
-                        const tree = JsonView.renderJSON(data, document.querySelector('#players'));
-                        JsonView.expandChildren(tree); // Expand tree after rendering   
-                    }}
-                )
+            function displayThisJson(data, player_id) {{
+                return () => {{
+                    let this_data = data[player_id];
+                    const tree = JsonView.renderJSON(this_data, document.querySelector('#json'));
+                    JsonView.expandChildren(tree); // Expand tree after rendering		
+                }}
+            }}
+            
+            function deletePlayer(friendly_key) {{
+                fetch(`/players/${{friendly_key}}` {{method: 'DELETE'}});
+            }}
+            
+            function setupTableHead(table) {{
+                let tHead = table.createTHead();
+                let row = tHead.insertRow();
+                let th = document.createElement("th");
+                th.appendChild(document.createTextNode("Players"));
+                th.appendChild(document.createTextNode("Delete"));
+                row.appendChild(th);
+            }}
+            
+            function setupTableRow(key, table) {{
+                let friendly_key = key.substring(5); // remove plr:+
+            
+                let row = table.insertRow();
+                // display info
+                let cell = row.insertCell();
+                let displayInfo = document.createElement("input");
+                displayInfo.type = "button";
+                displayInfo.value = `View ${{friendly_key}}`;
+                displayInfo.onclick = displayThisJson(data, key);
+                cell.appendChild(displayInfo);
+            
+                let cell = row.insertCell();
+                let deleteButton = document.createElement("input");
+                deleteButton.type = "button";
+                deleteButton.value = `Delete ${{friendly_key}}`;
+                deleteButton.onclick = deletePlayer(friendly_key);
+                cell.appendChild(deleteButton);
+            }}
+            
+            function setupTable(data) {{
+                let table = u("#players").nodes[0];
+                setupTableHead(table);
+                for (let key of Object.keys(data)) {{
+                        
+                }}
+            }}
+
+        fetch('/players').then(res => res.json()).then(data => setupTable(data));
         </script>
         </body>
         """
