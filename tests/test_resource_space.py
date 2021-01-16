93ee922886b1a5286ae63a0e86a61a413c1cae0d
Daniel Drexler <aeturnum@gmail.com>
2021-01-15 20:16:38 -0800

    Gross hack to make resource space work

--- tests/test_resource_space.py
+++ tests/test_resource_space.py
@@ -2,7 +2,8 @@ from spins_halp_line.media.resource_space import RSResource
 
 
 async def test_search():
-    search = await RSResource.for_room("Shipwreck Yard Front")
+    search = await RSResource.for_room("Tip Line Tip 1")
     print(search)
     for resource in search:
         print(resource._data)
+    assert False
